dc5c7c45a2b69ec9e24b45445376fcf723668235
Xorro78 <30311884+Xorro78@users.noreply.github.com>
2018-06-05 08:44:49 -0400

    Added white space afetr comma

--- Server.py
+++ Server.py
@@ -4,6 +4,6 @@ import socketserver
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("",PORT), Handler)
-print("Server at PORT  ",PORT)
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("Server at PORT  ", PORT)
 httpd.serve_forever()
