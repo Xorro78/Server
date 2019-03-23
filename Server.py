73ccb04e145e230b84f147d620f2d03bd47de745
Xorro78 <30311884+Xorro78@users.noreply.github.com>
2019-03-23 01:58:31 -0400

    Show Local IP

--- Server.py
+++ Server.py
@@ -1,9 +1,11 @@
 import http.server
 import socketserver
+import socket
 
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
+print("Local IP  ", socket.gethostbyname(socket.gethostname()))
 print("Server at PORT  ", PORT)
 httpd.serve_forever()
